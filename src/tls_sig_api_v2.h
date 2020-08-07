447be61063fbb996268b9cdee368c4f3c64836bc
yutingzeng <yutingzeng@tencent.com>
2020-08-07 19:50:54 +0800

    代码格式化,修改函数位置

--- src/tls_sig_api_v2.h
+++ src/tls_sig_api_v2.h
@@ -2,7 +2,7 @@
 #define TLS_SIG_API_V2_H
 
 #if defined(WIN32) || defined(WIN64)
-#pragma warning(disable: 4819)			// file codec warning, that's boring!
+#pragma warning(disable : 4819) // file codec warning, that's boring!
 #define TLS_API __declspec(dllexport)
 #else
 #define TLS_API
@@ -11,44 +11,27 @@
 #include <stdint.h>
 #include <string>
 
-enum {
-    CHECK_ERR01 = 1,       // sig 为空
-    CHECK_ERR02,           // sig base64 解码失败
-    CHECK_ERR03,           // sig zip 解压缩失败
-    CHECK_ERR04,           // sig 使用 json 解析时失败
-    CHECK_ERR05,           // sig 使用 json 解析时失败
-    CHECK_ERR06,           // sig 中 json 串 sig 字段 base64 解码失败
-    CHECK_ERR07,           // sig 中字段缺失
-    CHECK_ERR08,           // sig 校验签名失败，一般是秘钥不正确
-    CHECK_ERR09,           // sig 过期
-    CHECK_ERR10,           // sig 使用 json 解析时失败
-    CHECK_ERR11,           // sig 中 appid_at_3rd 与明文不匹配
-    CHECK_ERR12,           // sig 中 acctype 与明文不匹配
-    CHECK_ERR13,           // sig 中 identifier 与明文不匹配
-    CHECK_ERR14,           // sig 中 sdk_appid 与明文不匹配
-    CHECK_ERR15,           // sig 中 userbuf 异常
-    CHECK_ERR16,           // 内部错误
-    CHECK_ERR17,           // 签名失败 可能是私钥有误
-    CHECK_ERR_MAX,
+enum
+{
+        CHECK_ERR01 = 1, // sig 为空
+        CHECK_ERR02,     // sig base64 解码失败
+        CHECK_ERR03,     // sig zip 解压缩失败
+        CHECK_ERR04,     // sig 使用 json 解析时失败
+        CHECK_ERR05,     // sig 使用 json 解析时失败
+        CHECK_ERR06,     // sig 中 json 串 sig 字段 base64 解码失败
+        CHECK_ERR07,     // sig 中字段缺失
+        CHECK_ERR08,     // sig 校验签名失败，一般是秘钥不正确
+        CHECK_ERR09,     // sig 过期
+        CHECK_ERR10,     // sig 使用 json 解析时失败
+        CHECK_ERR11,     // sig 中 appid_at_3rd 与明文不匹配
+        CHECK_ERR12,     // sig 中 acctype 与明文不匹配
+        CHECK_ERR13,     // sig 中 identifier 与明文不匹配
+        CHECK_ERR14,     // sig 中 sdk_appid 与明文不匹配
+        CHECK_ERR15,     // sig 中 userbuf 异常
+        CHECK_ERR16,     // 内部错误
+        CHECK_ERR17,     // 签名失败 可能是私钥有误
+        CHECK_ERR_MAX,
 };
-/**用于生成实时音视频(TRTC)业务进房权限加密串,具体用途用法参考TRTC文档：https://cloud.tencent.com/document/product/647/32240 
- * TRTC业务进房权限加密串需使用用户定义的userbuf
- * @brief 生成 userbuf
- * @param account 用户名
- * @param dwSdkappid sdkappid
- * @param dwAuthID  数字房间号
- * @param dwExpTime 过期时间：该权限加密串的过期时间，建议300秒，300秒内拿到该签名，并且发起进房间操作
- * @param dwPrivilegeMap 用户权限，255表示所有权限
- * @param dwAccountType 用户类型,默认为0
- * @return byte[] userbuf
- */
-TLS_API std::string  gen_userbuf(
-	const std::string & account,
-	uint32_t dwSdkappid,
-	uint32_t dwAuthID,
-        uint32_t dwExpTime,
-	uint32_t dwPrivilegeMap,
-	uint32_t dwAccountType);
 /**
  *【功能说明】用于签发 TRTC 和 IM 服务中必须要使用的 UserSig 鉴权票据
  *
@@ -62,13 +45,12 @@ TLS_API std::string  gen_userbuf(
  * @return 0 为成功，非 0 为失败
  */
 TLS_API int genUserSig(
-        uint32_t sdkappid,
-        const std::string& userid,
-        const std::string& key,
-        int expire,
-        std::string& usersig,
-        std::string& errmsg);
-
+    uint32_t sdkappid,
+    const std::string &userid,
+    const std::string &key,
+    int expire,
+    std::string &usersig,
+    std::string &errmsg);
 
 /**
  *【功能说明】
@@ -100,16 +82,22 @@ TLS_API int genUserSig(
  * @return 0 为成功，非 0 为失败
  */
 TLS_API int genPrivateMapKey(
-        uint32_t sdkappid,
-        const std::string& userid,
-        const std::string& key,        
-        uint32_t roomid,
-        int expire,
-        int privilegeMap,
-        std::string& usersig,
-        std::string& errmsg);
+    uint32_t sdkappid,
+    const std::string &userid,
+    const std::string &key,
+    uint32_t roomid,
+    int expire,
+    int privilegeMap,
+    std::string &usersig,
+    std::string &errmsg);
 
-
+TLS_API std::string gen_userbuf(
+    const std::string &account,
+    uint32_t dwSdkappid,
+    uint32_t dwAuthID,
+    uint32_t dwExpTime,
+    uint32_t dwPrivilegeMap,
+    uint32_t dwAccountType);
 int thread_setup();
 void thread_cleanup();
 
--- src/tlssigapi_v2_tool.cpp
+++ src/tlssigapi_v2_tool.cpp
@@ -1,6 +1,6 @@
 #if defined(WIN32) || defined(WIN64)
-#pragma warning(disable: 4819)			// file codec warning, that's boring!
-#pragma warning(disable: 4099)
+#pragma warning(disable : 4819) // file codec warning, that's boring!
+#pragma warning(disable : 4099)
 #endif
 
 // 此文件演示了文件两个接口的使用方法
@@ -14,72 +14,36 @@
 #include <fstream>
 #include "tls_sig_api_v2.h"
 
-static void usage(const std::string& prog)
+static void usage(const std::string &prog)
 {
-    std::cout << "Usage:" << std::endl;
-    std::cout << "\tgen sig: " << prog << " gen key sig_file sdkappid identifier" << std::endl;
+	std::cout << "Usage:" << std::endl;
+	std::cout << "\tgen sig: " << prog << " gen key sig_file sdkappid identifier" << std::endl;
 	std::cout << "\tgen sig e.g.: " << prog << " gen 5bd2850fff3ecb11d7c805251c51ee463a25727bddc2385f3fa8bfee1bb93b5e sig 1400000000 xiaojun" << std::endl;
 	std::cout << "\tgenuser sig: " << prog << " genuser key sig_file sdkappid identifier userbuf" << std::endl;
 	std::cout << "\tgenuser sig: " << prog << " genuser 5bd2850fff3ecb11d7c805251c51ee463a25727bddc2385f3fa8bfee1bb93b5e sig 1400000000 xiaojun abc" << std::endl;
 }
 
-static int genUserSig(const std::string& key, const std::string& sig_file,
-		uint32_t sdkappid, const std::string& userid)
+static int genUserSig(const std::string &key, const std::string &sig_file,
+					  uint32_t sdkappid, const std::string &userid)
 {
-    std::string sig;
-    std::string err_msg;
-	int ret = genUserSig(sdkappid, userid, key, 180*86400, sig, err_msg);
-	if (0 != ret) {
-        std::cout << "error msg: " << err_msg << " return " << ret << std::endl;
-		return -3;
-	}
-
-#if defined(WIN32) || defined(WIN64)
-	FILE * sig_fp = NULL;
-	fopen_s(&sig_fp, sig_file.c_str(), "w+");
-#else
-	FILE* sig_fp = fopen(sig_file.c_str(),"w+");
-#endif
-	if (!sig_fp) {
-        std::cout << "open file " << sig_file << "failed" << std::endl;
-		return -4;
-	}
-
-	// 将签名写入文件
-	int written_cnt = (int)fwrite(sig.c_str(), sizeof(char), sig.size(), sig_fp);
-	if (sig.size() > (unsigned int)written_cnt && 0 != ferror(sig_fp))
+	std::string sig;
+	std::string err_msg;
+	int ret = genUserSig(sdkappid, userid, key, 180 * 86400, sig, err_msg);
+	if (0 != ret)
 	{
-		std::cout << "write sig content failed" << std::endl;
-		return -5;
-	}
-
-	std::cout << sig << std::endl;
-	std::cout << "generate sig ok" << std::endl;
-
-	return 0;
-}
-
-static int genPrivateMapKey(const std::string& key, const std::string& sig_file,
-		uint32_t sdkappid, const std::string& userid)
-{
-    std::string sig;
-    std::string err_msg;
-    int ret = genPrivateMapKey(sdkappid, userid, key,
-            10000,180*86400, 255, sig, err_msg);
-	if (0 != ret) {
-        std::cout << "error msg: " << err_msg << " return " << ret << std::endl;
+		std::cout << "error msg: " << err_msg << " return " << ret << std::endl;
 		return -3;
 	}
 
 #if defined(WIN32) || defined(WIN64)
-	FILE * sig_fp = NULL;
+	FILE *sig_fp = NULL;
 	fopen_s(&sig_fp, sig_file.c_str(), "w+");
 #else
-	FILE* sig_fp = fopen(sig_file.c_str(),"w+");
+	FILE *sig_fp = fopen(sig_file.c_str(), "w+");
 #endif
 	if (!sig_fp)
 	{
-        std::cout << "open file " << sig_file << "failed" << std::endl;
+		std::cout << "open file " << sig_file << "failed" << std::endl;
 		return -4;
 	}
 
@@ -97,40 +61,86 @@ static int genPrivateMapKey(const std::string& key, const std::string& sig_file,
 	return 0;
 }
 
-int main(int argc, char * argv[])
+static int genPrivateMapKey(const std::string &key, const std::string &sig_file,
+							uint32_t sdkappid, const std::string &userid)
 {
-    if (argc < 2) {
-        usage(argv[0]);
-        return -1;
-    }
+	std::string sig;
+	std::string err_msg;
+	int ret = genPrivateMapKey(sdkappid, userid, key,
+							   10000, 180 * 86400, 255, sig, err_msg);
+	if (0 != ret)
+	{
+		std::cout << "error msg: " << err_msg << " return " << ret << std::endl;
+		return -3;
+	}
 
-    const char * cmd = argv[1];
-    std::string sig_file;
-    std::string sdkappid_str;
-    std::string userid;
+#if defined(WIN32) || defined(WIN64)
+	FILE *sig_fp = NULL;
+	fopen_s(&sig_fp, sig_file.c_str(), "w+");
+#else
+	FILE *sig_fp = fopen(sig_file.c_str(), "w+");
+#endif
+	if (!sig_fp)
+	{
+		std::cout << "open file " << sig_file << "failed" << std::endl;
+		return -4;
+	}
 
-    int ret = 0;
-    if (0 == strcmp(cmd, "gen") && 6 == argc) {
-        std::string key = argv[2];
-        std::string sig_file = argv[3];
-        std::string sdkappid_str = argv[4];
-        std::string userid = argv[5];
-        ret = genUserSig(key, sig_file, strtol(sdkappid_str.c_str(), NULL, 10), userid);
-    } else if (0 == strcmp(cmd, "genuser") && 6 == argc) {
-        std::string key = argv[2];
-        std::string sig_file = argv[3];
-        std::string sdkappid_str = argv[4];
-        std::string userid = argv[5];
-        ret = genPrivateMapKey(key, sig_file,
-                strtol(sdkappid_str.c_str(), NULL, 10), userid);
-    } else {
-        usage(argv[0]);
-        return -1;
-    }
+	// 将签名写入文件
+	int written_cnt = (int)fwrite(sig.c_str(), sizeof(char), sig.size(), sig_fp);
+	if (sig.size() > (unsigned int)written_cnt && 0 != ferror(sig_fp))
+	{
+		std::cout << "write sig content failed" << std::endl;
+		return -5;
+	}
 
-    if (0 != ret) {
-        std::cout << "cmd " << cmd << " return " << ret << std::endl;
-    }
+	std::cout << sig << std::endl;
+	std::cout << "generate sig ok" << std::endl;
 
-    return ret;
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		usage(argv[0]);
+		return -1;
+	}
+
+	const char *cmd = argv[1];
+	std::string sig_file;
+	std::string sdkappid_str;
+	std::string userid;
+
+	int ret = 0;
+	if (0 == strcmp(cmd, "gen") && 6 == argc)
+	{
+		std::string key = argv[2];
+		std::string sig_file = argv[3];
+		std::string sdkappid_str = argv[4];
+		std::string userid = argv[5];
+		ret = genUserSig(key, sig_file, strtol(sdkappid_str.c_str(), NULL, 10), userid);
+	}
+	else if (0 == strcmp(cmd, "genuser") && 6 == argc)
+	{
+		std::string key = argv[2];
+		std::string sig_file = argv[3];
+		std::string sdkappid_str = argv[4];
+		std::string userid = argv[5];
+		ret = genPrivateMapKey(key, sig_file,
+							   strtol(sdkappid_str.c_str(), NULL, 10), userid);
+	}
+	else
+	{
+		usage(argv[0]);
+		return -1;
+	}
+
+	if (0 != ret)
+	{
+		std::cout << "cmd " << cmd << " return " << ret << std::endl;
+	}
+
+	return ret;
 }
